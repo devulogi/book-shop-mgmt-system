c5a9471b4b24c9bcf2138138f4add5e4e7ae20bb
devulogi <devulogijrp.github@gmail.com>
2023-01-24 06:08:01 +0800

    feat: create register route

---
+++ routes/register.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register' });
+}
+
+/* POST register a new user. */
+router.post('/register', function(req, res) {
+  // Check if the user already exists
+  User.findOne({ username: req.body.username }, function(err, user) {
+    if (err) {
+      return next(err);
+    }
+    if (user) {
+      req.flash('error', 'User already exists');
+      return res.redirect('/register');
+    }
+
+    // Create a new user
+    var newUser = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+
+    // Save the user
+    newUser.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+      req.flash('success', 'User created');
+      res.redirect('/login');
+    });
+  }
+});
