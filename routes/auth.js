4d3dbe13481dcc386acaae4896042f88d1a9bb0e
devulogi <devulogijrp.github@gmail.com>
2023-01-25 02:11:55 +0800

    update: setup messages

--- routes/auth.js
+++ routes/auth.js
@@ -4,15 +4,23 @@ var passport = require('passport');
 
 /* GET auth page. */
 router.get('/', function(req, res, next) {
-  res.render('auth', { title: 'Authentication' });
+  res.render('auth', { title: 'Authentication', page: 'auth' });
 });
 
 /* POST auth page. */
-router.post('/', passport.authenticate('local', {
-  successRedirect: '/',
-  successFlash: 'Welcome!',
-  failureRedirect: '/auth',
-  failureFlash: true
-}));
+router.post('/', function (req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err); }
+    if (!user) {
+      req.flash('error', info.message);
+      return res.redirect('/auth');
+    }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      req.flash('success', 'You have successfully logged in.');
+      return res.redirect('/');
+    });
+  })(req, res, next);
+});
 
 module.exports = router;
