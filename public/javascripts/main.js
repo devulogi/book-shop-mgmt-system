2f9d5fceb543fc47fcfc39daeef130de1d00e4e0
devulogi <devulogijrp.github@gmail.com>
2023-01-25 14:53:45 +0800

    update: hide message after 2 seconds

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,14 @@
 $(function() {
+  // Close message on click of close button
   $('.message .close')
     .on('click', function () {
       $(this)
         .closest('.message')
         .transition('fade');
     });
+
+  // Auto hide message after 2 seconds
+  setTimeout(function() {
+    $('.message').transition('fade');
+  }, 2000);
 });
